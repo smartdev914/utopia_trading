2010e4f2b641362767ac0ef99c99f0217f0008c4
codemachine914 <billjack0202@gmail.com>
2021-09-25 16:09:56 -0700

    add new charting library

---
+++ src/common/data/chartUtils/helpers.js
@@ -0,0 +1,43 @@
+// Make requests to CryptoCompare API
+export async function makeApiRequest(path) {
+    try {
+        const response = await fetch(`https://min-api.cryptocompare.com/${path}`)
+        return response.json()
+    } catch (error) {
+        throw new Error(`CryptoCompare request error: ${error.status}`)
+    }
+}
+
+export async function makeUtopiaApiRequest(path) {
+    try {
+        const response = await fetch(`https://price-retriever-dot-utopia-315014.uw.r.appspot.com/${path}`, {
+            headers: {
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+        })
+        return response.json()
+    } catch (error) {
+        throw new Error(`CryptoCompare request error: ${error.status}`)
+    }
+}
+
+// Generate a symbol ID from a pair of the coins
+export function generateSymbol(exchange, fromSymbol, toSymbol) {
+    const short = `${fromSymbol}/${toSymbol}`
+    return {
+        short,
+        full: `${exchange}:${short}`,
+    }
+}
+
+export function parseFullSymbol(fullSymbol) {
+    const match = fullSymbol.match(/^(\w+):(\w+)\/(\w+)$/)
+    if (!match) {
+        return null
+    }
+
+    return { exchange: match[1], fromSymbol: match[2], toSymbol: match[3] }
+}
