e4d1bafc791df10ae0ad183054fa615d85185811
codemachine914 <billjack0202@gmail.com>
2021-11-02 11:18:20 -0700

    update graph colors

--- src/components/DynamicTVSDark.jsx
+++ src/components/DynamicTVSDark.jsx
@@ -15,63 +15,18 @@ export default function DynamicTVS() {
                 interval: '15', // default interval
                 container: 'tv_chart_container',
                 datafeed: Datafeed,
+                disabled_features: ['use_localstorage_for_settings'],
                 library_path: '/libs/charting_library_cloned_data/charting_library/',
                 height: '100%',
                 width: '100%',
                 theme: 'Dark',
                 custom_css_url: '/assets/css/tradingView.css',
-                settings_adapter: {
-                    initialSettings: JSON.stringify({
-                        backgroundType: 'solid',
-                        background: 'rgba(21, 0, 53, 1)',
-                        backgroundGradientStartColor: '#150035',
-                        backgroundGradientEndColor: '#150035',
-                        vertGridProperties: {
-                            color: 'rgba(240, 243, 250, 0.06)',
-                            style: 0,
-                        },
-                        horzGridProperties: {
-                            color: 'rgba(240, 243, 250, 0.06)',
-                            style: 0,
-                        },
-                        crossHairProperties: {
-                            color: '#9598A1',
-                            style: 2,
-                            transparency: 0,
-                            width: 1,
-                            'color:': '#9598A1',
-                        },
-                        topMargin: 10,
-                        bottomMargin: 8,
-                        axisProperties: {
-                            autoScale: true,
-                            autoScaleDisabled: false,
-                            lockScale: false,
-                            percentage: false,
-                            percentageDisabled: false,
-                            indexedTo100: false,
-                            log: false,
-                            logDisabled: false,
-                            alignLabels: true,
-                            isInverted: false,
-                        },
-                        legendProperties: {
-                            showStudyArguments: true,
-                            showStudyTitles: true,
-                            showStudyValues: true,
-                            showSeriesTitle: true,
-                            showSeriesOHLC: true,
-                            showLegend: true,
-                            showBarChange: true,
-                            showBackground: true,
-                            backgroundTransparency: 50,
-                            wrapText: false,
-                        },
-                    }),
-                    setValue: (key, value) => {
-                        console.log({ key, value })
-                    },
-                    removeValue: (key) => {},
+                applyOverrides: true,
+                overrides: {
+                    'paneProperties.background': '#150035',
+                    'paneProperties.backgroundType': 'solid',
+                    'paneProperties.backgroundGradientStartColor': '#150035',
+                    'paneProperties.backgroundGradientEndColor': '#150035',
                 },
             })
         }
