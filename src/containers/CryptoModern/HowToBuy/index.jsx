9be5c76f0d8efbe9e8ed075c8bf65a12936e8bde
codemachine914 <billjack0202@gmail.com>
2021-12-10 07:37:26 -0800

    update how to buy

--- src/containers/CryptoModern/HowToBuy/index.jsx
+++ src/containers/CryptoModern/HowToBuy/index.jsx
@@ -18,22 +18,13 @@ const WalletPortal = () => (
                     <Fade down delay={400}>
                         <div className="stepOne stepWrapper">
                             <Fade clear delay={1000}>
-                                <Text as="p" className="step" content="Setup MetaMask (Computer/Laptop users) or TrustWallet (Phone users)" />
+                                <Text as="p" className="step" content="Setup MetaMask (Computer/Laptop users) and purchase BNB" />
                                 <div className="stepDesc">
+                                    <Text as="span" content="Download " />
                                     <a className="link" href="https://metamask.io/">
                                         MetaMask
                                     </a>
-                                    <Text as="span" content="&nbsp;can be added as a chrome browser extension or downloaded from the app/playstore and&nbsp;" />
-                                    <a className="link" href="https://trustwallet.com/">
-                                        TrustWallet
-                                    </a>
-                                    <Text
-                                        as="span"
-                                        content="&nbsp;can be downloaded from the play store. After that, for MetaMask, you will have to add the Binance Smart Chain to your network-list. "
-                                    />
-                                    <a className="link" href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain/">
-                                        (Click here for a step-by-step tutorial)
-                                    </a>
+                                    <Text as="span" content="and purchase BNB through a 3rd party on ramp service or a CEX like Binance/Kucoin and transfer them to your MetaMask" />
                                 </div>
                                 <div className="stepLine" />
                             </Fade>
@@ -43,13 +34,13 @@ const WalletPortal = () => (
                     <Fade down delay={400}>
                         <div className="stepTwo stepWrapper">
                             <Fade clear delay={1000}>
-                                <Text as="p" className="step" content="Buy and send BNB to your wallet" />
+                                <Text as="p" className="step" content="Exchange for Utopia" />
                                 <div className="stepDesc">
-                                    <Text
-                                        as="span"
-                                        content="Buy BNB on a centralised exchange (i.e. Binance, Coinbase, KuCoin etc.). Then using your wallet address, transfer the tokens to your wallet from the exchange. 
-                                        On TrustWallet, you can buy BNB directly to your wallet with fiat using Simplex. "
-                                    />
+                                    <Text as="span" content="Open " />
+                                    <a className="link" href="https://udex.co/">
+                                        Udex.co
+                                    </a>
+                                    <Text as="span" content="where Utopia is already loaded for swap. Then connect your BSC wallet and set slippage to 11-12%" />
                                 </div>
                                 <div className="stepLine" />
                             </Fade>
@@ -59,27 +50,10 @@ const WalletPortal = () => (
                     <Fade down delay={400}>
                         <div className="stepThree stepWrapper">
                             <Fade clear delay={1000}>
-                                <Text as="p" className="step" content="Swap BnB Smart Chain for Utopia on Pancakeswap" />
+                                <Text as="p" className="step" content="Approve the Swap" />
                                 <div className="stepDesc">
                                     <p>
-                                        <Text as="span" content="For computer/laptop users, navigate to&nbsp;" />
-                                        <a className="link" href="https://pancakeswap.finance/">
-                                            pancakeswap.finance
-                                        </a>
-                                        <Text
-                                            as="span"
-                                            content="&nbsp;and connect your MetaMask wallet. For phone users, access the Dapp browser within the trust wallet (android users only) or metamask app and navigate to&nbsp;"
-                                        />
-                                        <a className="link" href="https://pancakeswap.finance">
-                                            pancakeswap.finance
-                                        </a>
-                                        <Text as="span" content="&nbsp;and connect your wallet." />
-                                    </p>
-                                    <p>
-                                        <Text
-                                            as="span"
-                                            content="Once that is done, obtain the Utopia token address and paste into the pancakeswap exchange. Ensure to set slippage to 11% and if any error occurs try a higher slippage. Next swap BnB Smart Chain for Utopia token, which you should be able to view now in your wallet. To view Utopia token on MetaMask, you will have to manually add Utopia to your network list."
-                                        />
+                                        <Text as="span" content="Then approve the transaction and swap to Utopia! Make sure to add the Utopia token to you wallet so you can view it" />
                                     </p>
                                     {/* <p>
                                         <Text as="span" content="UTOPIA Token Address: " />
