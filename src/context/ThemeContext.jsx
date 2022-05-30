b3b11ad6bb472cabd7e014b2f44d7f37cb883c6b
codemachine914 <billjack0202@gmail.com>
2022-05-30 13:29:41 -0700

    fix toggling themes on home

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,14 +1,23 @@
+import { useRouter } from 'next/dist/client/router'
 import React, { useEffect, useState } from 'react'
 
 const ThemeContext = React.createContext()
 
 const ThemeContextProvider = ({ children }) => {
     const [currentTheme, toggleCurrentTheme] = useState('darkMode')
+    const router = useRouter()
+    const { pathname } = router
 
     useEffect(() => {
-        const theme = localStorage.getItem('theme')
-        toggleCurrentTheme(theme || 'darkMode')
-    }, [])
+        if (pathname === '/exchange') {
+            const theme = localStorage.getItem('theme')
+            toggleCurrentTheme(theme || 'darkMode')
+        } else {
+            document.querySelector('body').classList.remove('darkMode')
+            document.querySelector('body').classList.remove('lightMode')
+            document.querySelector('body').classList.remove('utopiaMode')
+        }
+    }, [pathname])
 
     useEffect(() => {
         localStorage.setItem('theme', currentTheme)
